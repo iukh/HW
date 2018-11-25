e00e05f0e3195eac00a7d918ef2e964bf71f03f8
iukh <33911363+iukh@users.noreply.github.com>
2018-11-26 02:14:00 +0300

    Update console.js

--- console.js
+++ console.js
@@ -2,34 +2,34 @@ function stringMessage() {
   return Array.prototype.join.call(arguments[0]," ");
 };
 console.log = (function(){
-  let _a = console.log;
+  let _tmpConsole = console.log;
   return function() {
     localStorage.setItem('Last Log', stringMessage(arguments))
-    return _a.apply(this,arguments);
+    return _tmpConsole.apply(this,arguments);
   }
 })();
 
 console.info = (function(){
-  let _a = console.info;
+  let _tmpConsole = console.info;
   return function() {
     localStorage.setItem('Last Info', stringMessage(arguments))
-    return _a.apply(this,arguments);
+    return _tmpConsole.apply(this,arguments);
   }
 })();
 
 console.warn = (function(){
-  let _a = console.warn;
+  let _tmpConsole = console.warn;
   return function() {
     localStorage.setItem('Last Warn', stringMessage(arguments))
-    return _a.apply(this,arguments);
+    return _tmpConsole.apply(this,arguments);
   }
 })();
 
 console.error = (function(){
-  let _a = console.error;
+  let _tmpConsole = console.error;
   return function() {
     localStorage.setItem('Last Error', stringMessage(arguments))
-    return _a.apply(this,arguments);
+    return _tmpConsole.apply(this,arguments);
   }
 })();
 
