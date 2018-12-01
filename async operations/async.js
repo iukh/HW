610f15955e27a87967ba5e4c8a1b5a61864c5a46
iukh <33911363+iukh@users.noreply.github.com>
2018-12-02 01:16:33 +0300

    Update async.js

--- async operations/async.js
+++ async operations/async.js
@@ -1,19 +1,3 @@
-const strg = localStorage;
-let imgList = [];
-for (let i = 0; i < strg.length; i++) {
-  key = strg.key(i);
-  imgList.push(strg.getItem(key));
-}
-for (var i =0; i < imgList.length; i ++) {
-  loadImage(imgList[i])
-  .then(function(url) {
-    $('.container').append('<img  class="item" src="'+url+'" />');
-  })
-  .catch(function(url) {
-    console.log("Cannot load the image by path: ", url);
-  });
-}
-
 function loadImage(url) {
   return new Promise(function(resolve, reject) {
     let img = new Image();
@@ -26,25 +10,60 @@ function loadImage(url) {
     img.src = url;
   });
 }
-
-$(document).ready(function(){
-  $(".openPopup").click(function() {
-    $('.selectItemPopup').css('display','block');
-  });
-  $(".closePopup").click(function() {
-    $('.selectItemPopup').css('display','none');
-  });
-  $(".addItem").click(function() {
-    let path = $(".selectAnimal").val();
-    imgList.push(path);
-    strg.setItem(Math.floor(Math.random(1,100)*100), path);
-    loadImage(path)
-    .then(function(url) {
-      $('.container').append('<img  class="item" src="'+url+'" />');
+function Animal(url, name) {
+  this.photo = url;
+  this.name = name;
+};
+Animal.prototype.showInformation = function(url,name) {
+  $('.container').append('<div class="block"><img  class="item" src="'+this.photo+'" /><h3>Nickname: '+ this.name + '</h3></div>');
+  $('.selectItemPopup').css('display','none');
+  $(".animalName").val('');
+};
+let popupActions = {
+  openPopup: function() {
+    $(".openPopup").click(function() {
+      $('.selectItemPopup').css('display','block');
+    });
+  },
+  addAnimal: function(imgList) {
+    $(".addItem").click(function() {
+      let path = $(".selectAnimal").val();
+      let name = $(".animalName").val();
+      imgList.push(path);
+      localStorage.setItem(name, path);
+      loadImage(path)
+      .then(function(url) {
+        new Animal(path,name).showInformation();
+      })
+      .catch(function(url) {
+        console.log("Cannot load the image by path:  ", url);
+      });
+    });
+  },
+  closePopup: function() {
+    $(".closePopup").click(function() {
       $('.selectItemPopup').css('display','none');
+    });
+  }
+};
+function loadAnimalsFromStorage() {
+  let imgList = [];
+  for (let i = 0; i < localStorage.length; i++) {
+    let name = localStorage.key(i);
+    imgList.push(localStorage.getItem(name));
+    loadImage(imgList[i])
+    .then(function(url) {
+      new Animal(imgList[i],name).showInformation();
     })
     .catch(function(url) {
-      console.log("Cannot load the image by path:  ", url);
+      console.log("Cannot load the image by path: ", url);
     });
-  });
+  }
+  return imgList;
+}
+$(document).ready(function(){
+  let imgList = loadAnimalsFromStorage();
+  popupActions.openPopup();
+  popupActions.addAnimal(imgList);
+  popupActions.closePopup();
 });
